8186c7ca270bd412b7bb22ba5d05d44f25df399a
prapeller <prapeller@mail.ru>
2023-11-07 22:19:53 +0300

    feat: film_bookmarks.py  public / auth

--- api_ugc/main.py
+++ api_ugc/main.py
@@ -1,18 +1,17 @@
 from contextlib import asynccontextmanager
-from pathlib import Path
 
 import fastapi as fa
-import sentry_sdk
 import uvicorn
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
 import core.dependencies
-from api.v1.comment_likes import router as comment_likes_router
-from api.v1.film_bookmarks import router as film_bookmarks_router
-from api.v1.film_comments import router as film_coments_router
-from api.v1.film_ratings import router as film_ratings_router
-from api.v1.user_film_progress import router as user_film_progress_router
+from api.v1.auth.comment_likes import router as comment_likes_router
+from api.v1.auth.film_bookmarks import router as film_bookmarks_router
+from api.v1.auth.film_comments import router as film_comments_router
+from api.v1.auth.film_ratings import router as film_ratings_router
+from api.v1.auth.user_film_progress import router as user_film_progress_router
+from api.v1.public.film_bookmarks import router as film_bookmarks_router_public
 from core.config import settings
 from core.logger_config import setup_logger
 
@@ -54,12 +53,13 @@ async def log_request_id(request: fa.Request, call_next):
 
 v1_router_auth = fa.APIRouter()
 v1_router_auth.include_router(user_film_progress_router, prefix='/user-film-progress', tags=['user_film_progress'])
-v1_router_auth.include_router(film_coments_router, prefix='/film-comments', tags=['film_comments'])
+v1_router_auth.include_router(film_comments_router, prefix='/film-comments', tags=['film_comments'])
 v1_router_auth.include_router(film_ratings_router, prefix='/film-ratings', tags=['film_ratings'])
 v1_router_auth.include_router(film_bookmarks_router, prefix='/film-bookmarks', tags=['film_bookmarks'])
 v1_router_auth.include_router(comment_likes_router, prefix='/comment-likes', tags=['comment_likes'])
 
 v1_router_public = fa.APIRouter()
+v1_router_public.include_router(film_bookmarks_router_public, prefix='/film-bookmarks', tags=['film_bookmarks'])
 
 app.include_router(v1_router_auth, prefix="/api/v1")
 app.include_router(v1_router_public, prefix="/api/v1")
