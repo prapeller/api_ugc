62be5a90453b83275ff251e185498d49acfb7015
prapeller <prapeller@mail.ru>
2023-11-07 22:20:20 +0300

    chore: logging not needed for now

--- api_ugc/core/logger_config.py
+++ api_ugc/core/logger_config.py
@@ -1,10 +1,11 @@
 import logging
 from pathlib import Path
 
-import logstash
 
-from core.config import settings
-from core.shared import RequestIdFilter
+# import logstash
+#
+# from core.config import settings
+# from core.shared import RequestIdFilter
 
 
 def setup_logger(service_name: str, service_dir: Path):
@@ -21,8 +22,14 @@ def setup_logger(service_name: str, service_dir: Path):
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
-    logger.addHandler(
-        logstash.LogstashHandler(settings.LOGSTASH_HOST, settings.LOGSTASH_PORT, version=1, tags=['api_ugc']))
-    logger.addFilter(RequestIdFilter())
+    # logger.addHandler(
+    #     logstash.LogstashHandler(settings.LOGSTASH_HOST, settings.LOGSTASH_PORT, version=1, tags=['api_ugc']))
+    # logger.addFilter(RequestIdFilter())
 
     return logger
+
+
+SERVICE_DIR = Path(__file__).resolve().parent
+SERVICE_NAME = SERVICE_DIR.stem
+
+logger = setup_logger(SERVICE_NAME, SERVICE_DIR)
